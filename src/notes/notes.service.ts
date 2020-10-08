691de6c64b6ae7a4b29725fd8c5f6f07ed016470
Zoran <zoransasko@inbox.com>
2020-10-08 22:25:58 +0200

    small refactor

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,44 +1,37 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Note } from './note.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Note } from './note.entity';
 
 @Injectable()
-export class NotesService {
+export class NotesService {
+  constructor(
+    @InjectRepository(Note) private notesRepository: Repository<Note>,
+  ) {}
+  async getNotes(): Promise<Note[]> {
+    return await this.notesRepository.find();
+  }
 
-    constructor(
-        @InjectRepository(Note) 
-        private notesRepository: Repository<Note>
-    ){}
-    
-    async getNotes(): Promise<Note[]> {
-        return await this.notesRepository.find();
-    }
+  findOne(id: string): Promise<Note> {
+    return this.notesRepository.findOne(id);
+  }
 
-    findOne(id: string): Promise<Note> {
-        return this.notesRepository.findOne(id);
-    }
+  async createNote(note: Note) {
+    this.notesRepository.save(note);
+  }
 
-    async createNote(note: Note) {
-        this.notesRepository.save(note);
-    }
+  async remove(id: string): Promise<void> {
+    await this.notesRepository.delete(id);
+  }
 
-    async remove(id: string): Promise<void>{
-        await this.notesRepository.delete(id);
-    }
-
-    async editNote(
-        id: number,
-        note: Note,
-      ): Promise<Note> {
-        const editedNote = await this.notesRepository.findOne(id);
-        if (!editedNote) {
-          throw new NotFoundException('Note is not found');
-        }
-        editedNote.description = note.description;
-        editedNote.title = note.title;
-        await editedNote.save();
-        return editedNote;
-    }
-
+  async editNote(id: number, note: Note): Promise<Note> {
+    const editedNote = await this.notesRepository.findOne(id);
+    if (!editedNote) {
+      throw new NotFoundException('Note is not found');
+    }
+    editedNote.description = note.description;
+    editedNote.title = note.title;
+    await editedNote.save();
+    return editedNote;
+  }
 }
